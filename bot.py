fc51262d7ba0c5276d183ffb6a77102913c459a1
kulanbekova <ulanbekova_ka@auca.kg>
2023-11-25 21:03:05 +0600

    Add face detector for photo in bot

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+import cv2
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext, MessageHandler, filters
 
 TOKEN = '6668204272:AAEB-jUZuEyDuOnDEjaDRJKzYvryJ6Qjw7E'
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -15,8 +17,25 @@ async def start(update: Update, context: CallbackContext) -> None:
 async def handle_images(update: Update, context: CallbackContext) -> None:
     if update.message.photo:
         file_id = update.message.photo[-1].file_id
-        file = context.bot.get_file(file_id)
-        await update.message.reply_text('Image received!')
+        file = await context.bot.get_file(file_id)
+
+        photo_path = 'received_image.jpg'
+        await file.download_to_drive(photo_path)
+
+        image = cv2.imread(photo_path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+        # Draw rectangles around faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Save the image with rectangles
+        output_path = 'image_with_faces.jpg'
+        cv2.imwrite(output_path, image)
+        with open(output_path, 'rb') as photo:
+            await update.message.reply_photo(photo, caption='Faces detected!')
+
     else:
         await update.message.reply_text('Please send an image.')
 
