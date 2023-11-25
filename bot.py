0ed5e324b6d27734fd1b5441a76694c598bb1e8d
kulanbekova <ulanbekova_ka@auca.kg>
2023-11-25 20:43:52 +0600

    Add photo handler for bot

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext, MessageHandler, filters
 
 TOKEN = '6668204272:AAEB-jUZuEyDuOnDEjaDRJKzYvryJ6Qjw7E'
 
@@ -12,9 +12,20 @@ async def start(update: Update, context: CallbackContext) -> None:
     )
 
 
+async def handle_images(update: Update, context: CallbackContext) -> None:
+    if update.message.photo:
+        file_id = update.message.photo[-1].file_id
+        file = context.bot.get_file(file_id)
+        await update.message.reply_text('Image received!')
+    else:
+        await update.message.reply_text('Please send an image.')
+
+
 def main() -> None:
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.PHOTO, handle_images))
+
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
