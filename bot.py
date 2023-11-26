40b132337aacabb9062874a145769cbc7c9e8d15
kulanbekova <ulanbekova_ka@auca.kg>
2023-11-26 22:48:02 +0600

    Add emotions classification to bot

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import cv2
+from deepface import DeepFace
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext, MessageHandler, filters
 
 TOKEN = '6668204272:AAEB-jUZuEyDuOnDEjaDRJKzYvryJ6Qjw7E'
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+model = DeepFace.build_model("Emotion")
+emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -26,15 +29,26 @@ async def handle_images(update: Update, context: CallbackContext) -> None:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-        # Draw rectangles around faces
+        emotions_detected = []
         for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            face_roi = gray[y:y + h, x:x + w]
+            resized_face = cv2.resize(face_roi, (48, 48), interpolation=cv2.INTER_AREA)
+            normalized_face = resized_face / 255.0
+            reshaped_face = normalized_face.reshape(1, 48, 48, 1)
+            preds = model.predict(reshaped_face)[0]
+            emotion_idx = preds.argmax()
+            emotion = emotion_labels[emotion_idx]
+            emotions_detected.append(emotion)
+
+            # Draw rectangle around face and label with predicted emotion
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(image, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
         # Save the image with rectangles
         output_path = 'image_with_faces.jpg'
         cv2.imwrite(output_path, image)
         with open(output_path, 'rb') as photo:
-            await update.message.reply_photo(photo, caption='Faces detected!')
+            await update.message.reply_photo(photo, caption=f'Emotions detected: {", ".join(emotions_detected)}')
 
     else:
         await update.message.reply_text('Please send an image.')
