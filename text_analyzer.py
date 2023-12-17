1ce24d5da9f5c190f3f0a3cf23c6573bda3d4806
kulanbekova <ulanbekova_ka@auca.kg>
2023-12-17 22:26:38 +0600

    Learning about text analyzing ways

--- text_analyzer.py
+++ text_analyzer.py
@@ -2,12 +2,13 @@ from transformers import pipeline
 
 
 def detect_basic_emotion(text):
-    classifier = pipeline('sentiment-analysis', model='nlptown/bert-base-multilingual-uncased-sentiment')
-    emotions = classifier(text)
-    top_emotion = emotions[0]['label']
-    return top_emotion
+    classifier = pipeline('ner', model='dbmdz/bert-large-cased-finetuned-conll03-english')
+    entities = classifier(text)
+    entity_labels = [entity['entity'] for entity in entities]
+    basic_emotion = ', '.join(entity_labels)
+    return basic_emotion
 
 
-example_text = "I feel really happy about the good news!"
+example_text = "I am feeling happy and excited!"
 basic_emotion = detect_basic_emotion(example_text)
-print(f"Basic emotion: {basic_emotion}")
+print(f"Basic emotions: {basic_emotion}")
