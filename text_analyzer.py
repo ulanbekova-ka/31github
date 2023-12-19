e552f7c4adf845a18ec0c9be1e990faf8e7b0680
kulanbekova <ulanbekova_ka@auca.kg>
2023-12-19 23:17:44 +0600

    Get top 3 emotions in each book by its description text

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def load_emotion_lexicon(file_path):
     lexicon = {}
     with open(file_path, 'r') as file:
@@ -10,7 +13,7 @@ def load_emotion_lexicon(file_path):
 
 
 def calculate_emotion_score(text, emotion_lexicon):
-    words = text.split()
+    words = str(text).split()
     emotion_scores = {emotion: 0 for emotion in emotion_lexicon[list(emotion_lexicon.keys())[0]].keys()}
 
     for word in words:
@@ -21,12 +24,21 @@ def calculate_emotion_score(text, emotion_lexicon):
     return emotion_scores
 
 
+book_data = pd.read_csv("book_data.csv")
+
 nrc_lexicon_path = 'NRC-Emotion-Lexicon-Wordlevel-v0.92.txt'
 nrc_lexicon = load_emotion_lexicon(nrc_lexicon_path)
 
-text = "I am so happy now!"
-emotion_scores = calculate_emotion_score(text, nrc_lexicon)
 
-print("NRC Emotion Lexicon Scores:")
-for emotion, score in emotion_scores.items():
-    print(f"{emotion}: {score}")
+for index, row in book_data.iterrows():
+    book_description = row['book_desc']
+    emotion_scores = calculate_emotion_score(book_description, nrc_lexicon)
+
+    sorted_emotion_scores = sorted(emotion_scores.items(), key=lambda x: x[1], reverse=True)
+    top_3_emotions = sorted_emotion_scores[:3]
+
+    print(f"Book Title: {row['book_title']}")
+    print("Top 3 Emotions:")
+    for emotion, score in top_3_emotions:
+        print(f"{emotion}: {score}")
+    print("\n")
