caa5ac43b55357b16b814ada61f95ba1a4dce076
kulanbekova <ulanbekova_ka@auca.kg>
2023-12-21 23:27:54 +0600

    Consider genres in recommending

--- text_analyzer.py
+++ text_analyzer.py
@@ -12,7 +12,7 @@ def load_emotion_lexicon(file_path):
     return lexicon
 
 
-def calculate_emotion_score(text, emotion_lexicon):
+def calculate_emotion_scores(text, emotion_lexicon):
     words = str(text).split()
     emotion_scores = {emotion: 0 for emotion in emotion_lexicon[list(emotion_lexicon.keys())[0]].keys()}
 
@@ -30,27 +30,36 @@ nrc_lexicon_path = 'NRC-Emotion-Lexicon-Wordlevel-v0.92.txt'
 nrc_lexicon = load_emotion_lexicon(nrc_lexicon_path)
 
 
-def get_top_books_by_emotion(book_data, emotion, top_n=5):
+def get_top_books_by_emotion_rating_and_genre(book_data, emotion, genre, top_n=5):
     book_emotion_scores = []
 
     for index, row in book_data.iterrows():
         book_description = row['book_desc']
-        emotion_scores = calculate_emotion_score(book_description, nrc_lexicon)
+        emotion_scores = calculate_emotion_scores(book_description, nrc_lexicon)
         book_rating = row['book_rating']
+        book_genres = str(row['genres'])  # Convert to string to handle NaN values
 
-        if book_rating >= 4:
-            book_emotion_scores.append({
-                'book_title': row['book_title'],
-                'emotion_score': emotion_scores[emotion],
-                'book_rating': book_rating
-            })
+        if pd.notna(book_rating) and pd.notna(book_genres) and book_rating >= 4:
+            # Check if any genre in the book's genres matches the specified genre
+            if any(g.lower() == genre.lower() for g in book_genres.split('|')):
+                book_emotion_scores.append({
+                    'book_title': row['book_title'],
+                    'emotion_score': emotion_scores[emotion],
+                    'book_rating': book_rating,
+                    'book_genres': book_genres
+                })
 
+    # Sort books based on emotion scores, then by book rating, and finally by genre match
     sorted_books = sorted(book_emotion_scores, key=lambda x: (x['emotion_score'], x['book_rating']), reverse=True)
 
-    print(f"Top {top_n} Books with Highest {emotion.capitalize()} Scores and Ratings:")
+    # Print the top books with the highest emotion scores, ratings, and matching genre
+    print(f"Top {top_n} Books with Highest {emotion.capitalize()} Scores, Ratings (Rating >= 4), and Matching Genre ({genre}):")
     for i, book_info in enumerate(sorted_books[:top_n]):
-        print(f"{i + 1}. {book_info['book_title']} - {emotion.capitalize()} Score: {book_info['emotion_score']}, Rating: {book_info['book_rating']}")
+        print(f"{i + 1}. {book_info['book_title']} - {emotion.capitalize()} Score: {book_info['emotion_score']}, Rating: {book_info['book_rating']}, Genres: {book_info['book_genres']}")
 
+# Specify the emotion and genre you want to analyze
+target_emotion = 'sadness'  # Replace with the desired emotion (e.g., 'joy', 'anger', 'surprise', etc.)
+target_genre = 'Dystopia'  # Replace with the desired genre
 
-target_emotion = 'fear'
-get_top_books_by_emotion(book_data, target_emotion)
+# Get and print the top books by the specified emotion, highest rating, and matching genre
+get_top_books_by_emotion_rating_and_genre(book_data, target_emotion, target_genre)
